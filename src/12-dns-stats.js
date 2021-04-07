11cb1ad370944f9620badaf5a03d203e4cb51aff
Andrey <univer1857@gmail.com>
2021-04-07 19:05:13 +0300

    feat: task12 solution

--- src/12-dns-stats.js
+++ src/12-dns-stats.js
@@ -20,8 +20,39 @@
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new Error('Not implemented');
+function getDNSStats(domains) {
+  let firstLevel;
+  let firstLevelCount = 0;
+  let secondLevel;
+  let thirdLevel;
+  let secondLevelCount = 0;
+  const thirdLevelCount = 1;
+  let secondLevelStr = '';
+  let thirdLevelStr = '';
+  const results = {};
+
+  if (domains.length === 0) {
+    return results;
+  }
+  for (let i = 0; i < domains.length; i++) {
+    [firstLevel] = domains[i].match(/\.[a-z]+$/);
+    [, secondLevel] = domains[i].match(/([a-z]+)\.[a-z]{3}$/);
+    [, thirdLevel] = domains[i].match(/([a-z]+)\.[a-z]+\.[a-z]{3}$/) || '';
+    if (firstLevel) {
+      firstLevelCount++;
+    }
+    if (secondLevel) {
+      secondLevelCount++;
+    }
+  }
+  secondLevelStr += `${firstLevel}.${secondLevel}`;
+  results[firstLevel] = firstLevelCount;
+  results[secondLevelStr] = secondLevelCount;
+  if (thirdLevel) {
+    thirdLevelStr += `${firstLevel}.${secondLevel}.${thirdLevel}`;
+    results[thirdLevelStr] = thirdLevelCount;
+  }
+  return results;
 }
 
 module.exports = getDNSStats;
