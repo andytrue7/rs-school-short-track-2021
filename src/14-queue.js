cb72b5f3d98e4425b968c55b5383a1f84d642cac
Andrey <univer1857@gmail.com>
2021-04-09 17:53:38 +0300

    feat: task14 solution

--- src/14-queue.js
+++ src/14-queue.js
@@ -1,4 +1,4 @@
-// const ListNode = require('../extensions/list-node');
+const ListNode = require('../extensions/list-node');
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
  *
@@ -10,6 +10,9 @@
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  *
  */
+
+const node = new ListNode();
+node.element = 0;
 
 class Queue {
   constructor() {
